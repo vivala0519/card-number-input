99639421103f97f0f9006a9ff4e79bc4ccfb36fa
vivala0519 <vivala0519@naver.com>
2023-03-14 14:00:36 +0900

    [feat] update cardCode, ExpMonthYear structure

--- src/component/CardCode.tsx
+++ src/component/CardCode.tsx
@@ -4,11 +4,8 @@ const CardCode = () => {
     const inputTitle = 'CVC';
     const inputStyle = {border: '0px', width: '90%', outline: 'none', textAlign: 'center'} as React.CSSProperties;
     return (
-        <div>
-            <p>{inputTitle}</p>
-            <div style={{ border: '1px solid', borderRadius: '3px', height: '30px' }}>
-                <input style={ inputStyle }/>
-            </div>
+        <div className="span-2" style={{ height: '30px' }}>
+            <input placeholder={ inputTitle } style={ inputStyle }/>
         </div>
     );
 }
--- src/component/ExpMonthYear.tsx
+++ src/component/ExpMonthYear.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 const ExpMonthYear = () => {
     const inputTitle = 'Expiration Data';
-    const inputStyle = {border: '0px', width: '45%', outline: 'none', textAlign: 'center'} as React.CSSProperties;
+    const inputStyle = {border: '0px', width: '30%', outline: 'none', textAlign: 'center'} as React.CSSProperties;
     return (
-        <div>
-            <p>{inputTitle}</p>
-            <div style={{ display: 'flex', border: '1px solid', borderRadius: '3px', height: '30px' }}>
-                <input style={ inputStyle }/>
-                <span>/</span>
-                <input style={ inputStyle }/>
-            </div>
+        <div className='span-2' style={{ display: 'flex', height: '30px' }}>
+            <input placeholder={ inputTitle } style={{ border: '0px', width: '30%', outline: 'none', textAlign: 'center' }}/>
+            <input style={ inputStyle }/>
+            <span>/</span>
+            <input style={ inputStyle }/>
         </div>
     );
 }
